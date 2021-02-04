17846cc28c0e81984f0658b6e240472f44d62f5c
jayarghargh <jayarghargh@gmail.com>
2021-02-04 19:21:50 +1030

    added middleware link to server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const path = require('path');
-const tempNotes = {
-	name: "justin",
-	wants: "code",
-	cares: "doggo"
-};
+let notesArray = require('./db/db.json');
 
 const app = express();
 const PORT = 3000;
@@ -15,8 +11,11 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 // api
-app.get('/api/notes', (req, res) => res.json(tempNotes));
-app.post('/api/notes', ((req, res) => res.json(tempNotes)));
+app.get('/api/notes', (req, res) => res.json(notesArray));
+app.post('/api/notes', ((req, res) => res.json(notesArray)));
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public/')))
 
 // Listener
 app.listen(PORT, () => console.log(`I am here in port ${PORT}`));
